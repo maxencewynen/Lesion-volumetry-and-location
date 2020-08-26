1ece4286464952a9254dbc1d4c20c75753144d42
Maxence Wynen <maxencewynen@gmail.com>
2020-08-26 16:55:21 +0200

    Deleted some comments

--- edit_aseg.py
+++ edit_aseg.py
@@ -33,16 +33,3 @@ def update_aseg(subject):
     nifti_out = nib.Nifti1Image(aseg_mask,affine=aseg_image.affine)
     nib.save(nifti_out, SUBJECTS_DIR +"/sub-{0}_MPRAGE.nii/mri/aseg_lesions.nii.gz".format(subject))   
     nib.save(nifti_out, MAIN_DIR + "/sub-{0}/segmentations/sub-{0}_aseg_lesions.nii.gz".format(subject))   
-
-"""    sp.run(["bash", "/home/mwynen/setup_env.sh"], shell=True)
-    sp.run(["mri_convert", SUBJECTS_DIR +"/sub-{0}_MPRAGE.nii/mri/aseg_lesions.nii.gz".format(subject), 
-             SUBJECTS_DIR +"/sub-{0}_MPRAGE.nii/mri/aseg_lesions.mgz".format(subject)], shell=True)
-
-    mri_segstats = "mri_segstats --seed 1234 --seg /home/mwynen/freesurfer/subjects/sub-{0}_MPRAGE.nii/mri/aseg_lesions.mgz --sum \
-        /home/mwynen/freesurfer/subjects/sub-{0}_MPRAGE.nii/stats/aseg_lesions.stats --pv /home/mwynen/freesurfer/subjects/sub-{0}_MPRAGE.nii/mri/norm.mgz --empty \
-            --brainmask /home/mwynen/freesurfer/subjects/sub-{0}_MPRAGE.nii/mri/brainmask.mgz --brain-vol-from-seg --excludeid 0 \
-                --excl-ctxgmwm --supratent --subcortgray --in /home/mwynen/freesurfer/subjects/sub-{0}_MPRAGE.nii/mri/norm.mgz \
-                    --in-intensity-name norm --in-intensity-units MR --etiv --surf-wm-vol --surf-ctx-vol \
-                        --totalgray --euler --ctab /home/mwynen/freesurfer/ASegStatsLUT.txt --subject sub-{0}_MPRAGE.nii" 
-    mri_segstats = mri_segstats.format(subject)
-    sp.run(mri_segstats.split())"""
