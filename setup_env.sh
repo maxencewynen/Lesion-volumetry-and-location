201a28d8c563169b03e3f50d6a2837aba5d2f9bc
Maxence Wynen <maxencewynen@gmail.com>
2020-08-28 17:10:05 +0200

    All Subjects

--- setup_env.sh
+++ setup_env.sh
@@ -4,3 +4,5 @@ export SUBJECTS_DIR=$FREESURFER_HOME/subjects
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export MAIN_DIR=/home/mwynen/scripts/MultipleSclerosis
 export ANTSPATH=/home/mwynen/softwares/install/bin
+export ALL_SUBJECTS=(010 023 039)
+#ALL_SUBJECTS=(006 010 012 013 019 023 034 035 036 037 038 039 040 041 042 043 044 045 047 048 051 052 058 059 097 098)
