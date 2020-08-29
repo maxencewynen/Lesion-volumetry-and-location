3d7639f0473eb200a9de41abe9162109c370e119
Maxence Wynen <maxencewynen@gmail.com>
2020-08-30 00:18:29 +0200

    Added Threshold environment variable

--- setup_env.sh
+++ setup_env.sh
@@ -8,5 +8,6 @@ export ALL_SUBJECTS=(010 023 039)
 export LES_VOLOC_DIR=/home/mwynen/scripts
 export SAMSEG=/home/mwynen/freesurfer/python/scripts/run_samseg
 export ANTs_registration=/home/mwynen/softwares/ANTs/Scripts/antsRegistrationSyNQuick.sh
+export THRESHOLD=0.5
 #ALL_SUBJECTS=(006 010 012 013 019 023 034 035 036 037 038 039 040 041 042 043 044 045 047 048 051 052 058 059 097 098)
 alias les_voloc='source $LES_VOLOC_DIR/fill_db.sh'
