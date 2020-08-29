4d4533f8328451bef1abc059fdb9235da01fadc4
Maxence Wynen <maxencewynen@gmail.com>
2020-08-30 00:20:25 +0200

    changed fill_db to les_voloc

--- setup_env.sh
+++ setup_env.sh
@@ -10,4 +10,4 @@ export SAMSEG=/home/mwynen/freesurfer/python/scripts/run_samseg
 export ANTs_registration=/home/mwynen/softwares/ANTs/Scripts/antsRegistrationSyNQuick.sh
 export THRESHOLD=0.5
 #ALL_SUBJECTS=(006 010 012 013 019 023 034 035 036 037 038 039 040 041 042 043 044 045 047 048 051 052 058 059 097 098)
-alias les_voloc='source $LES_VOLOC_DIR/fill_db.sh'
+alias les_voloc='source $LES_VOLOC_DIR/les_voloc.sh'
